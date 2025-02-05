b96e5ba008a12a6b99352ca6d24d255441612af3
egortabula <egor.vnl@gmail.com>
2025-02-05 20:39:52 +0300

    feat: enhance module binding and view structure with improved imports and const usage

--- brics/easy_module/__brick__/lib/app/modules/{{module_name.snakeCase()}}/{{module_name.snakeCase()}}_binding.dart
+++ brics/easy_module/__brick__/lib/app/modules/{{module_name.snakeCase()}}/{{module_name.snakeCase()}}_binding.dart
@@ -1,5 +1,7 @@
 import '{{module_name.snakeCase()}}_controller.dart';
 import '{{module_name.snakeCase()}}_repository.dart';
+import 'package:{{package_name.snakeCase()}}/core/utils/helpers/go_router_binding.dart';
+import 'package:get/get.dart';
 
 class {{module_name.pascalCase()}}Binding extends GoRouterBinding {
   @override
