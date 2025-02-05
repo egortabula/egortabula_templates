b1c0c66aca09d7f11255f4d9856e88e94eb74a2e
egortabula <egor.vnl@gmail.com>
2025-02-05 20:39:44 +0300

    feat: implement pre-generation logic to retrieve package name from current directory

--- brics/easy_module/hooks/pre_gen.dart
+++ brics/easy_module/hooks/pre_gen.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  // TODO: add pre-generation logic.
+  // Get the current directory
+  final currentDirectory = Directory.current;
+
+  // Return the folder name
+  final packageName = currentDirectory.path.split(Platform.pathSeparator).last;
+
+  context.vars['package_name'] = packageName;
 }
