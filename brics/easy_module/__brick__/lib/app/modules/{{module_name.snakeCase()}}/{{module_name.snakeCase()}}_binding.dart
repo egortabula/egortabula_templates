d4963522eabf9267f104c79af8cdf5223b9e1127
egortabula <egor.vnl@gmail.com>
2025-02-05 20:43:05 +0300

    feat: integrate GoRouter for improved state management in module binding

--- brics/easy_module/__brick__/lib/app/modules/{{module_name.snakeCase()}}/{{module_name.snakeCase()}}_binding.dart
+++ brics/easy_module/__brick__/lib/app/modules/{{module_name.snakeCase()}}/{{module_name.snakeCase()}}_binding.dart
@@ -2,6 +2,8 @@ import '{{module_name.snakeCase()}}_controller.dart';
 import '{{module_name.snakeCase()}}_repository.dart';
 import 'package:{{package_name.snakeCase()}}/core/utils/helpers/go_router_binding.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+
 
 class {{module_name.pascalCase()}}Binding extends GoRouterBinding {
   @override
@@ -10,7 +12,7 @@ class {{module_name.pascalCase()}}Binding extends GoRouterBinding {
   }
 
   @override
-  void injectBindings(state) {
+  void injectBindings(GoRouterState state) {
     Get.put(
       {{module_name.pascalCase()}}Controller(
         repository: {{module_name.pascalCase()}}Repository(),
