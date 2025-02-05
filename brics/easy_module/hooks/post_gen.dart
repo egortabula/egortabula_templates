bb2cf8328408a54529567045015883bbee20e0d3
egortabula <egor.vnl@gmail.com>
2025-02-05 22:35:01 +0300

    feat: implement post-generation logic to run 'dart fix' command

--- brics/easy_module/hooks/post_gen.dart
+++ brics/easy_module/hooks/post_gen.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  final dartFixProgress = context.logger.progress('Running dart fix...');
+
+  // Run `flutter packages get` after generation.
+  await Process.run('dart', [
+    'fix --apply',
+  ]);
+
+  dartFixProgress.complete();
 }
