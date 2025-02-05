7f9d19f8f9cc5b3ea70c1252370bf9e1fc517ff0
egortabula <egor.vnl@gmail.com>
2025-02-05 23:02:11 +0300

    feat: add internationalization file generation for specified languages

--- brics/easy_module/hooks/post_gen.dart
+++ brics/easy_module/hooks/post_gen.dart
@@ -3,6 +3,24 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
+   final moduleName = Platform.environment['MODULE_NAME'];
+  final languages = Platform.environment['LANGUAGES']?.split(',') ?? ['en', 'ru'];
+
+  final i18nDir = Directory('assets/i18n/');
+  if (!i18nDir.existsSync()) {
+    i18nDir.createSync(recursive: true);
+  }
+
+  for (final lang in languages) {
+    final fileName = '${moduleName}_$lang.i18n.json';
+    final file = File('${i18nDir.path}/$fileName');
+    
+    if (!file.existsSync()) {
+      file.writeAsStringSync('{\n  "example_key": "Example translation for $lang"\n}');
+      print('✅ Created $fileName');
+    }
+  }
+  
   final dartFixProgress = context.logger.progress('Running dart fix...');
 
   final result = await Process.run(
