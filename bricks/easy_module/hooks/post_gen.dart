d63b58868821db124ae45c98509d81f291cd1841
egortabula <egor.vnl@gmail.com>
2025-02-07 01:24:05 +0300

    feat: update brick version and implement dart fix in post generation hook

--- bricks/easy_module/hooks/post_gen.dart
+++ bricks/easy_module/hooks/post_gen.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-Future<void> run(HookContext context) async {}
+Future<void> run(HookContext context) async {
+  final dartFixProgress = context.logger.progress('Running dart fix...');
+
+  await Process.run(
+    'dart',
+    ['fix', '--apply', '.'],
+    runInShell: true,
+  );
+
+  dartFixProgress.complete();
+}
