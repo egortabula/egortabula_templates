b1cccf7a0ea7b36a46f82cc51317f20551aff139
egortabula <egor.vnl@gmail.com>
2025-02-07 01:06:55 +0300

    feat: simplify post generation hook by removing dart fix logic

--- bricks/easy_module/hooks/post_gen.dart
+++ bricks/easy_module/hooks/post_gen.dart
@@ -1,25 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  final dartFixProgress = context.logger.progress('Running dart fix...');
-
-  final result = await Process.run(
-    'dart',
-    ['fix', '--apply'],
-    runInShell: true,
-  );
-
-  if (result.exitCode == 0) {
-    context.logger
-        .progress('✅ dart fix --apply completed successfully.')
-        .complete();
-  } else {
-    context.logger
-        .progress('❌ dart fix --apply failed: ${result.stderr}')
-        .complete();
-  }
-
-  dartFixProgress.complete();
-}
+Future<void> run(HookContext context) async {}
