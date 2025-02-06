208087f46a5f70df5743eb6100e332f01730820e
egortabula <egor.vnl@gmail.com>
2025-02-06 03:24:22 +0300

    feat: add support for specifying supported locales and create corresponding i18n file

--- brics/easy_module/hooks/post_gen.dart
+++ brics/easy_module/hooks/post_gen.dart
@@ -3,26 +3,6 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final moduleName = context.vars['module_name'];
-  final languages = context.vars['languages'];
-
-  context.logger.detail('$languages, type: ${languages.runtimeType}');
-
-  final i18nDir = Directory('assets/i18n/');
-  if (!i18nDir.existsSync()) {
-    i18nDir.createSync(recursive: true);
-  }
-
-  for (final lang in languages) {
-    final fileName = '{{module_name.snakeCase()}}_$lang.i18n.json';
-    final file = File('${i18nDir.path}/$fileName');
-
-    if (!file.existsSync()) {
-      file.writeAsStringSync('{\n}');
-      context.logger.success('✅ Created $fileName');
-    }
-  }
-
   final dartFixProgress = context.logger.progress('Running dart fix...');
 
   final result = await Process.run(
