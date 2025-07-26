c23f668f4051bdf896ef786b40a133a0b1af9809
Egor <egor.vnl@gmail.com>
2025-07-27 01:54:11 +0300

    feat(shared_prefs_async_storage): export Storage and StorageException from storage package

--- storage_client/__brick__/storage_client/{{#include_shared_prefs_async}}shared_prefs_async_storage{{/include_shared_prefs_async}}/lib/shared_prefs_async_storage.dart
+++ storage_client/__brick__/storage_client/{{#include_shared_prefs_async}}shared_prefs_async_storage{{/include_shared_prefs_async}}/lib/shared_prefs_async_storage.dart
@@ -1,4 +1,6 @@
 /// Async storage that saves data in device's persistent memory using SharedPreferences
 library;
 
+export 'package:storage/storage.dart' show Storage, StorageException;
+
 export 'src/shared_prefs_async_storage.dart';
